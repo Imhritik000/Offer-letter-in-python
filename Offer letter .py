de5b0712739c418a5eb4e3757e6d7c4ededdb6b7
Imhritik000 <hritikch000@gmail.com>
2024-09-09 15:48:04 +0530

    Add files via upload

---
+++ Offer letter .py
@@ -0,0 +1,10 @@
+name= input("enter candidate name : ")
+position = input("enter candidate position : ")
+ctc = input("enter candidate ctc : ")
+hr_name = input("enter hr name : " )
+offer_letter = f""" Hi {name},
+you have been selected for the {position} position,
+your CTC will be {ctc})
+thanks and regard,
+{hr_name}."""
+print(offer_letter)
